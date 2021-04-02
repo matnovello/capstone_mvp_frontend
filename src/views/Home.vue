30a08fc701e92c2ff94ec107d9907e3fcd6a0c1d
Mat <mvello66@gmail.com>
2021-04-02 13:06:56 -0400

    adds axios dependencies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
+
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  created: function () {},
+  methods: {
+  getInfo: function() {
+    axios.get######
   }
-}
+  },
+};
 </script>
+
