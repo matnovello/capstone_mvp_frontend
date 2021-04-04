197c16f8972747479aa4b12c98da6aa49fd3eea2
Mat <mvello66@gmail.com>
2021-04-04 11:00:43 -0400

    adds front-end game creation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,10 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    
+    
+    <input type = text v-model="userName" placeholder="enter username">
+    <button v-on:click="getInfo">click and check console</button> 
   </div>
 </template>
 
@@ -16,13 +20,19 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      userName: "",
     };
   },
   created: function () {},
   methods: {
-  getInfo: function() {
-    axios.get######
-  }
+    getInfo: function () {
+      var params = {
+        user_name: this.userName,
+      };
+      axios.post("http://localhost:3000/api/games", params).then((response) => {
+        console.log(response.data);
+      });
+    },
   },
 };
 </script>
