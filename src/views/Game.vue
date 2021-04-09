935b96215a73113729740500912b52ea1d4ce868
Mat <mvello66@gmail.com>
2021-04-08 21:07:48 -0400

    adds 'move forward' which updates game.room ID

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -3,7 +3,8 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    {{ gameId }}
+    {{ test }}
+    <button v-on:click="moveForward">Move Forward</button>
     
   </div>
 </template>
@@ -19,17 +20,21 @@ export default {
     return {
       message: "You are in the woods",
       gameId: localStorage.getItem("gameId"),
+      test: "",
     };
   },
   created: function () {
-    // var params = {
-    //   gameId,
-    // };
-    // axios.get("http://localhost:3000/api/games/" + gameId).then((response) => {
-    //   console.log(response.data);
-    // });
+    axios.get(`http://localhost:3000/api/games/${this.gameId}`).then((response) => {
+      console.log(response.data);
+    });
   },
-  methods: {},
+  methods: {
+    moveForward: function () {
+      axios.patch(`http://localhost:3000/api/games/${localStorage.getItem("gameId")}`).then((response) => {
+        this.gameId = response.data.gameId;
+      });
+    },
+  },
 };
 </script>
 
