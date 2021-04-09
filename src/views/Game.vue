f55c65fe80002dceae83b97265a49d8b947c360f
Mat <mvello66@gmail.com>
2021-04-08 22:21:29 -0400

    adds purposeful error so i dont forget the next steps

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>{{ message }} {{ user }} </h1>
     {{ test }}
     <button v-on:click="moveForward">Move Forward</button>
     
@@ -18,14 +18,16 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "You are in the woods",
+      message: "Welcome to the game, ",
       gameId: localStorage.getItem("gameId"),
       test: "",
+      user: "",
     };
   },
   created: function () {
     axios.get(`http://localhost:3000/api/games/${this.gameId}`).then((response) => {
       console.log(response.data);
+      this.user = response.data.user.name;
     });
   },
   methods: {
@@ -33,7 +35,9 @@ export default {
       axios.patch(`http://localhost:3000/api/games/${localStorage.getItem("gameId")}`).then((response) => {
         this.gameId = response.data.gameId;
         console.log(response.data.current_room);
+        this.message = "you move forward into the next room..";
       });
+       HEY. you should focus on the back end,, focus on creating the monster model and attaching it to the rooms at random !
     },
   },
 };
